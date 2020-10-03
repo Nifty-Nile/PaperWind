d532d7de6fb709eba9f830c34d2e18ad25a8ba9f
kartik_sachu <sachinkartik166@gmail.com>
2020-10-03 16:17:30 +0530

    SLIDE ACTIVITY DONE... only function is done.. touchup is still left.. after getting all the assets from rahul will do that

--- app/src/main/java/com/nbird/paperwind/MainActivity.java
+++ app/src/main/java/com/nbird/paperwind/MainActivity.java
@@ -3,6 +3,7 @@ package com.nbird.paperwind;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.Html;
 import android.view.View;
@@ -12,100 +13,12 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ViewPager slideViewPager;
-    private LinearLayout dotLayout;
-    private SliderAdapter sliderAdapter;
-    private TextView[] mDots;
-    private Button nextbutton,backbutton;
-    private int currentPage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        slideViewPager=(ViewPager) findViewById(R.id.slideViewPager);
-        dotLayout=(LinearLayout) findViewById(R.id.dotLayout);
-        nextbutton=(Button) findViewById(R.id.button5);
-        backbutton=(Button) findViewById(R.id.button4);
-
-
-        sliderAdapter=new SliderAdapter(this);
-        slideViewPager.setAdapter(sliderAdapter);
-
-        addDotsIndicator(0);
-        slideViewPager.addOnPageChangeListener(viewListner);
-
-        nextbutton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                slideViewPager.setCurrentItem(currentPage+1);
-            }
-        });
-        backbutton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                slideViewPager.setCurrentItem(currentPage-1);
-            }
-        });
-
+        Intent intent=new Intent(getBaseContext(),SlideActivity.class);
+        startActivity(intent);
     }
-
-    public void addDotsIndicator(int position){
-        mDots=new TextView[3];
-        dotLayout.removeAllViews();
-        for(int i=0;i<mDots.length;i++){
-            mDots[i]=new TextView(this);
-            mDots[i].setText(Html.fromHtml("&#8226"));
-            mDots[i].setTextSize(35);
-            mDots[i].setTextColor(getResources().getColor(R.color.colorAccent));
-            dotLayout.addView(mDots[i]);
-
-        }
-        if(mDots.length>0){
-            mDots[position].setTextColor(getResources().getColor(R.color.colorPrimary));
-        }
-    }
-    ViewPager.OnPageChangeListener viewListner=new ViewPager.OnPageChangeListener() {
-        @Override
-        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-
-        }
-
-        @Override
-        public void onPageSelected(int position) {
-            addDotsIndicator(position);
-            currentPage=position;
-            if(currentPage==0){
-                nextbutton.setEnabled(true);
-                backbutton.setEnabled(false);
-                backbutton.setVisibility(View.INVISIBLE);
-
-                nextbutton.setText("Next");
-                backbutton.setText("");
-            }
-            else if(position==mDots.length-1){
-                nextbutton.setEnabled(true);
-                backbutton.setEnabled(true);
-                backbutton.setVisibility(View.VISIBLE);
-
-                nextbutton.setText("Finish");
-                backbutton.setText("Back");
-            }
-
-            else {
-                nextbutton.setEnabled(true);
-                backbutton.setEnabled(true);
-                backbutton.setVisibility(View.VISIBLE);
-
-                nextbutton.setText("Next");
-                backbutton.setText("Back");
-            }
-        }
-
-        @Override
-        public void onPageScrollStateChanged(int state) {
-
-        }
-    };
 }
