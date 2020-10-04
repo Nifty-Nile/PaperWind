19c2b5ef6872d009569f41c29a80e496a8295b28
kartik_sachu <sachinkartik166@gmail.com>
2020-10-04 12:54:15 +0530

    SLIDE ACTIVITY DONE... only function is done.. touchup is still left.. after getting all the assets from rahul will do that

--- app/src/main/java/com/nbird/paperwind/LoginActivity.java
+++ app/src/main/java/com/nbird/paperwind/LoginActivity.java
@@ -2,13 +2,25 @@ package com.nbird.paperwind;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
+    Button movesignup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        movesignup=(Button) findViewById(R.id.movesignup);
+
+        movesignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(getBaseContext(),SignUpActivity.class);
+            }
+        });
     }
 }
