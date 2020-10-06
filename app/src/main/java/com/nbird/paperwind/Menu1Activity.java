f0e6bdcb901588a47dc90a8da836a77591767afa
Aakash Tyagi <aakashtyagi232@gmail.com>
2020-10-06 11:12:59 +0530

    exam list updated

--- app/src/main/java/com/nbird/paperwind/Menu1Activity.java
+++ app/src/main/java/com/nbird/paperwind/Menu1Activity.java
@@ -121,18 +121,18 @@ return true;
     }
 
     public void parto(){
-        lstExam.add(new Exam("Jee Advance",R.drawable.back2,"Joint Entrance Examination – Advanced, formerly the Indian Institutes of Technology-Joint Entrance Examination, is an academic examination held annually in India."));
-        lstExam.add(new Exam("Jee Mains",R.drawable.back14,"Joint Entrance Examination – Advanced, formerly the Indian Institutes of Technology-Joint Entrance Examination, is an academic examination held annually in India."));
-        lstExam.add(new Exam("NEET",R.drawable.back2,"Joint Entrance Examination – Advanced, formerly the Indian Institutes of Technology-Joint Entrance Examination, is an academic examination held annually in India."));
-        lstExam.add(new Exam("Vitee",R.drawable.back1,"Joint Entrance Examination – Advanced, formerly the Indian Institutes of Technology-Joint Entrance Examination, is an academic examination held annually in India."));
-        lstExam.add(new Exam("Jee Advance",R.drawable.back2,"Joint Entrance Examination – Advanced, formerly the Indian Institutes of Technology-Joint Entrance Examination, is an academic examination held annually in India."));
-        lstExam.add(new Exam("Jee Mains",R.drawable.back14,"Joint Entrance Examination – Advanced, formerly the Indian Institutes of Technology-Joint Entrance Examination, is an academic examination held annually in India."));
-        lstExam.add(new Exam("NEET",R.drawable.back2,"Joint Entrance Examination – Advanced, formerly the Indian Institutes of Technology-Joint Entrance Examination, is an academic examination held annually in India."));
-        lstExam.add(new Exam("Vitee",R.drawable.back1,"Joint Entrance Examination – Advanced, formerly the Indian Institutes of Technology-Joint Entrance Examination, is an academic examination held annually in India."));
-        lstExam.add(new Exam("Jee Advance",R.drawable.back2,"Joint Entrance Examination – Advanced, formerly the Indian Institutes of Technology-Joint Entrance Examination, is an academic examination held annually in India."));
-        lstExam.add(new Exam("Jee Mains",R.drawable.back14,"Joint Entrance Examination – Advanced, formerly the Indian Institutes of Technology-Joint Entrance Examination, is an academic examination held annually in India."));
-        lstExam.add(new Exam("NEET",R.drawable.back2,"Joint Entrance Examination – Advanced, formerly the Indian Institutes of Technology-Joint Entrance Examination, is an academic examination held annually in India."));
-        lstExam.add(new Exam("Vitee",R.drawable.back1,"Joint Entrance Examination – Advanced, formerly the Indian Institutes of Technology-Joint Entrance Examination, is an academic examination held annually in India."));
+        lstExam.add(new Exam("JEE Advanced",R.drawable.back2,"Joint Entrance Examination – Advanced, which replaces IIT-JEE, is an annual examination for admissions to the prestigious IITs of India."));
+        lstExam.add(new Exam("JEE Main",R.drawable.back14,"Joint Entrance Examination – Main is a national level entrance exam conducted by NTA to offer admission to BE/BTech, BPlan, BArch courses at IIITs, NITs"));
+        lstExam.add(new Exam("NEET",R.drawable.back2,"The National Eligibility cum Entrance Test (Undergraduate), formerly AIPMT, is an entrance examination for UG medical courses(MBBS) and dental courses(BDS) in India."));
+        lstExam.add(new Exam("VITEEE",R.drawable.back1,"VITEEE is an annual entrance examination conducted by Vellore Institute of technology, a private university in Vellore, Tamil Nadu founded in 1984."));
+        lstExam.add(new Exam("COMED-K",R.drawable.back2,"State level entrance examination by the Consortium of Medical, Engineering and Dental colleges of Karnataka for admissions to Engineerting and Architecture courses."));
+        lstExam.add(new Exam("NDA",R.drawable.back14,"NDA exam is conducted by UPSC twice a year for admissions to Army, Navy, and Air Force wings of the prestigious National Defence Academy, Pune."));
+        lstExam.add(new Exam("BITSAT",R.drawable.back2,"BITS Admission test is an online entrance examination for admissions to integrated first degree programmes of BITS Pilani, Goa and Hyderabad."));
+        lstExam.add(new Exam("KVPY",R.drawable.back1,"Kishore Vaigyanik Protsahan Yojana is a scholarship program aimed at encouraging students to take up research careers in the areas of basic sciences by the Indian government."));
+        lstExam.add(new Exam("MHCET",R.drawable.back2,"MHT CET (MH CET) or Maharashtra Common Entrance Test is conducted by the State Common Entrance Test Cell for admissions to BE/BTech and Pharmacy programmes (BPharma/PharmaD)."));
+        lstExam.add(new Exam("SRMJEE",R.drawable.back14,"The SRM University conducts SRM Joint Engineering Entrance Examination (SRMJEEE) for granting admissions in undergraduate engineering courses. "));
+        lstExam.add(new Exam("X Boards",R.drawable.back2,"A public examination conducted by various boards of education including CISCE, CBSE and state boards for students of class 10 (Senior class)."));
+        lstExam.add(new Exam("XII Boards",R.drawable.back1,"The AISSCE, also called Boards in common language, is an examination conducted every year by the Central Board of Secondary Education on behalf of the Government of India"));
 
     }
 
