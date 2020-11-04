b8202e61ef1c42b32fa48b819dfe03c82a1a14b6
kartik_sachu <sachinkartik166@gmail.com>
2020-11-04 11:52:35 +0530

    dark mode and help section half done

--- app/src/main/java/com/nbird/paperwind/Menu1Activity.java
+++ app/src/main/java/com/nbird/paperwind/Menu1Activity.java
@@ -21,6 +21,7 @@ import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ListView;
